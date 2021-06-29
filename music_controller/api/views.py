99c62235956e94f5b9888f15cf6dde231a9e50f0
sb <stephane.baudry.vanaa@gmail.com>
2021-06-29 23:37:32 +0200

    Addied code to leave room and go back to HomePage

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -103,4 +103,25 @@ class UserInRoom(APIView):
         #serialize and return the python dictionary
         return JsonResponse(data, status=status.HTTP_200_OK)
 
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        if 'room_code' in self.request.session:
+            self.request.session.pop('room_code')
+            host_id = self.request.session.session_key
+            room_results = Room.objects.filter(host=host_id)
+            if len(room_results) > 0:
+                room = room_results[0]
+                room.delete()
+
+        return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
+
+### extra ###
+class SessionInfo(APIView):
+    def get(self, request, format=None):
+        session_info = {}
+        for key, value in request.session.items():
+            session_info[key] = value
+        session_info['session_key'] = self.request.session.session_key
+        return JsonResponse(session_info, status=status.HTTP_200_OK)
+
 
